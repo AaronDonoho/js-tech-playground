20e847aa110212b21af82ddce3b0b900f6f3bb9b
roborotoroto <work@roth.ninja>
2022-03-27 00:37:45 -0500

    Hello Svelte w/ Map

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Map from './map/Map.svelte';
+</script>
+
+<main>
+	<section class="header">
+		<h1>VDC</h1>
+	</section>
+	<Map></Map>
+</main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-rows: 1fr min-content;
+	}
+
+	.header {
+		height: 4rem;
+		line-height: 4rem;
+		padding: 0 1rem;
+		box-shadow: 0 5px 15px rgba(0, 0, 0, .1);
+	}
+
+	h1 {
+		color: #3d64ad;
+		text-transform: uppercase;
+		font-size: 2rem;
+		font-weight: 600;
+		margin: 0;
+	}
+</style>
---
+++ src/map/Map.svelte
@@ -0,0 +1,23 @@
+<script>
+  import * as mapboxgl from "mapbox-gl";
+  import { onMount } from "svelte";
+  import 'mapbox-gl/dist/mapbox-gl.css';
+
+  onMount(() => {
+    mapboxgl.accessToken = "pk.eyJ1Ijoicm9ib3JvdG9yb3RvIiwiYSI6ImNsMTdka3A2OTFvMHczam4xNnA1aDFycTUifQ.ceWbgCpz9W67WrWuNW8bDw";
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  });
+</script>
+
+<div id="map" />
+
+<style>
+  #map {
+    height: calc(100vh - 64px);
+  }
+</style>
